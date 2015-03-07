c1e84112b1637abd2c5176030e048dfda178b777
Arkadiusz Żmudzin <arkus777@gmail.com>
2015-03-07 15:48:39 +0100

    Initial commit

---
+++ app/src/main/java/pl/e_arkus/recievebroadcast/RecieveBroadcast.java
@@ -0,0 +1,16 @@
+package pl.e_arkus.recievebroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class RecieveBroadcast extends BroadcastReceiver {
+    public RecieveBroadcast() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Broadcast has been recieved", Toast.LENGTH_LONG).show();
+    }
+}
